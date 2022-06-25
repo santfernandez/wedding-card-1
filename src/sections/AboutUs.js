f16b5209c617bc5f7cbc5c45eb131544c75be2f4
santfernandez <you@example.com>
2022-06-25 18:54:28 -0300

    changed images for better compressed ones

--- src/sections/AboutUs.js
+++ src/sections/AboutUs.js
@@ -9,21 +9,21 @@ const AboutUs = () => {
         <div className="about-us-images-row-a">
           <img
             className="about-us-image"
-            src={`https://drive.google.com/uc?export=view&id=1LEiHDydZutzE3fhKjY80TUQgPTKNCepM`}
+            src={`https://drive.google.com/uc?export=view&id=114WKg_DWDmOtG4JnkFYRkhT80KFgUVUi`}
           />
           <img
             className="about-us-image"
-            src={`https://drive.google.com/uc?export=view&id=1DgRbFor20OiaaLNgkaTusVbAQErZ_jMX`}
+            src={`https://drive.google.com/uc?export=view&id=1kG5uw35q6__-N8yrr7QZECjXNSnD5I4Y`}
           />
         </div>
         <div className="about-us-images-row-b">
           <img
             className="about-us-image"
-            src={`https://drive.google.com/uc?export=view&id=1zhelvu5I9EZB9e-kWk4cgV1ZytzYuLRT`}
+            src={`https://drive.google.com/uc?export=view&id=1uiAe0AD7DgUBKqXn8aBWsINSmgw_pDp6`}
           />
           <img
             className="about-us-image"
-            src={`https://drive.google.com/uc?export=view&id=1SNihb9dKo34PayexaSqiHLGymA-HYn0T`}
+            src={`https://drive.google.com/uc?export=view&id=1Ql4OS5HnawNGtLh77iXpSju0918_GRrN`}
           />
         </div>
       </div>
