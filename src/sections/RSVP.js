dea2241c7b19d72078dfb7dde5a178d5de2eb53f
santfernandez <you@example.com>
2022-06-25 19:52:58 -0300

    changed rsvp button style

--- src/sections/RSVP.js
+++ src/sections/RSVP.js
@@ -7,11 +7,12 @@ const RSVP = () => {
       <div className="rsvp">
         <h2 className="rsvp-title">Asistencia</h2>
         <p className="rsvp-info">Confirmar antes del 30 julio</p>
-        <button type="button">
+        <button type="rsvp-button">
           <a
             target="_blank"
             rel="noreferrer"
             href="https://docs.google.com/forms/d/e/1FAIpQLSfBsJ9zCOEgW-dCjGXtaWwsoYTkPw1UZbe3Uay7L00ZkTFhiw/viewform"
+            className="rsvp-link"
           >
             Confirmar
           </a>
